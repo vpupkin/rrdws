e626bf06cd4e446b5f0f5cacf30612799cee8983
DrHudson <vasIlIIj.pupkin@googleMAIl.com>
2015-04-10 10:00:49 +0200

    2-my9080 ++

---
+++ 2-my9080.sh
@@ -0,0 +1,2 @@
+echo STARTING::mvn -o -e help:active-profiles tomcat:redeploy -Dcccache -Dmaven.test.skip=true -DCI-TC7-on-9080
+mvn -o -e help:active-profiles tomcat:redeploy -Dcccache -Dmaven.test.skip=true -DCI-TC7-on-9080
