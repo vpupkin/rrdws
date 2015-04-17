2f19ff15d033bb6f4fd205e0740c83e055d4377d
Ge Sha <gesha@uniposta.de>
2015-04-17 11:49:03 +0200

    fixed test.rrd && speed.gif @ tomcat7

---
+++ 2MY8080.sh
@@ -0,0 +1,2 @@
+echo  mvn -o -e help:active-profiles tomcat:redeploy -Dcccache -Dmaven.test.skip=true -DMY8080
+mvn -o -e help:active-profiles tomcat:redeploy -Dcccache -Dmaven.test.skip=true -DMY8080
