75479717be15850b02bbc9dc4e5a05129f2f880f
Vasja Pupkin <vasiliij.pupkin_at@GoogleMaIl.cOm>
2016-02-22 19:57:41 +0100

    temporary merged with sources from cachefs4j

---
+++ src/test/java/ri/cache/BasicCacheFactory.java
@@ -0,0 +1,18 @@
+package ri.cache;
+ 
+
+/** 
+ * <b>javax.cache.API </b>
+ * @author      gennady<br>
+ * <br>
+ * <b>Copyright:</b>     Copyright (c) 2006-2008 Monster AG <br>
+ * <b>Company:</b>       Monster AG  <br>
+ * 
+ * Creation:  Mar 31, 2011::9:11:50 PM<br> 
+ */
+public class BasicCacheFactory extends cc.co.llabor.cache.BasicCacheFactory{
+ 
+}
+
+
+ 
